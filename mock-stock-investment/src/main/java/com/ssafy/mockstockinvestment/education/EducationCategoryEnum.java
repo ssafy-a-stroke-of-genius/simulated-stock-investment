a84706257f248b88a2735579d50fc9ddb2191002
dhmonukim24 <dhmonukim24@gmail.com>
2023-11-09 09:18:25 +0900

    comment: education category enum 주석 생성 #9

--- mock-stock-investment/src/main/java/com/ssafy/mockstockinvestment/education/EducationCategoryEnum.java
+++ mock-stock-investment/src/main/java/com/ssafy/mockstockinvestment/education/EducationCategoryEnum.java
@@ -1,5 +1,9 @@
 package com.ssafy.mockstockinvestment.education;
 
+/**
+ * 모든 Enum은 필드명을 영어로 변경할 것
+ * EducationCategoryEnum => 카테고리 재구성 예정
+ */
 public enum EducationCategoryEnum {
-    금융,경제,부동산
+    금융,경제,부동산 //임시
 }
